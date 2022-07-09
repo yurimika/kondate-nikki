0285b6aafd8b72df90923722d98f94885394acdc
EC2 Default User <yurimikan2017@gmail.com>
2022-07-09 16:41:55 +0000

    create user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  devise_for :admins
+  devise_for :users
+
+  root 'public/homes#top'
+  namespace :public do
+    resources :users, only:[:show, :edit, :update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
