a1ace068eaa5c56a319ec7c3dee0fe69e7c85026
EC2 Default User <yurimikan2017@gmail.com>
2022-07-26 20:11:34 +0000

    add design 2

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
     @menus = Menu.all
-    @menus = Menu.all.order(created_at: :desc).limit(5)
-    @ranks = Menu.find(Like.group(:menu_id).where(created_at: Time.current.all_week).order('count(menu_id) desc').limit(5).pluck(:menu_id))
+    @menus = Menu.all.order(created_at: :desc).limit(6)
+    @ranks = Menu.find(Like.group(:menu_id).where(created_at: Time.current.all_week).order('count(menu_id) desc').limit(3).pluck(:menu_id))
   end
 
 
